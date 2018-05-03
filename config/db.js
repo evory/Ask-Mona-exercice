c276be49a6afe2f1ac692e028ad51f421c5e2eb5
Bryan BRANDT <bbrandt@e1r5p13.42.fr>
2018-05-03 18:35:44 +0200

    switch db connection data

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host     : 'eu-cdbr-west-02.cleardb.net',
-    user     : 'bb56737d2dcc6d',
-    password : '6f96a0a0',
+    host     : 'localhost',
+    user     : 'root',
+    password : 'br200991',
     multipleStatements: true
 });
 connection.connect((err) => {
@@ -11,7 +11,7 @@ connection.connect((err) => {
         throw err;
     } else {        
         connection.changeUser({
-            database : 'heroku_d5c02d25f68e641'
+            database : 'askmonadb'
         }, function(err) {
             if (err) {
             console.log('error in changing database', err);
